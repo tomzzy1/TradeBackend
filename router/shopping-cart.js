ca33c4f7fc0b713f2bfff25f7a93b32d73ac43ab
tomzzy <tomzzy@live.com>
2021-06-25 18:19:43 +0800

    fix bugs of cart

--- router/shopping-cart.js
+++ router/shopping-cart.js
@@ -6,30 +6,6 @@ let cart = new Router({
     prefix: '/cart'
 })
 
-/*cart.get('/list', async (ctx) => {
-    let goodsList = []
-    pool.query("SELECT id, number from shopping_cart", (err, result, field)=>{
-        console.log('shopping_cart get list')
-        console.log(result)  
-        for (let i = 0; i < result.length; i++)
-            {
-                pool.query("SELECT id, name, date, price FROM goods WHERE id=?", result[i].id, (err, goods, field) => {
-                if (err)
-                    console.log(err)
-                    console.log(goods)
-                    goods.number = result[i].number
-                    goodsList.push(goods)
-            })
-            
-            ctx.body = {
-                code: 20000,
-                data: 
-                {
-                    list: goodsList,
-                    total: goodsList.length
-                }
-            }
-        }*/
 cart.get('/list', async (ctx) => {
     //let goodsList = []
     let [cart, _] = await pool.query("SELECT id, number from shopping_cart")
@@ -61,15 +37,16 @@ cart.get('/list', async (ctx) => {
         }
     }
 
-
-
 }).post('/update', async (ctx) => {
     let postData = await parsePostData(ctx)
+    if (!postData)
+        console.log('error!')
     let parsedData = JSON.parse(postData)
     console.log(parsedData)
-    let id = parsedData['id']
-    let new_num = parsedData['number']
-    pool.query("UPDATE shopping_cart SET number=? WHERE id=?", new_num, id, (err, result, field)=>{
+    let id = parsedData.id
+    let new_num = parsedData.num
+    console.log(id, new_num)
+    pool.query("UPDATE shopping_cart SET number=? WHERE id=?", [new_num, id], (err, result, field)=>{
         console.log("The number of ID: {0} is updated to {1}".format(id, new_num))
     })
     ctx.body = 
@@ -80,10 +57,9 @@ cart.get('/list', async (ctx) => {
     let postData = await parsePostData(ctx)
     let parsedData = JSON.parse(postData)
     console.log(parsedData)
-    let id = parsedData['id']
-    pool.query("DELETE FROM shopping_cart WHERE id=?", id, (err, result, field)=>{
-        console.log(result)
-    })
+    let id = parsedData
+    let result = await pool.query("DELETE FROM shopping_cart WHERE id=?", id)
+    console.log(result)
     ctx.body = 
     {
         code: 20000
@@ -94,7 +70,7 @@ cart.get('/list', async (ctx) => {
     let parsedData = JSON.parse(postData)
     console.log(parsedData)
     let id = parsedData['id']
-    pool.query("INSERT INTO shopping_cart (id, number) VALUES(?, ?)", id, 1, (err, result, field)=>{
+    pool.query("INSERT INTO shopping_cart (id, number) VALUES(?, ?)", [id, 1], (err, result, field)=>{
         console.log(result)
     })
     ctx.body = 
