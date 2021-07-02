f196f80b61cf4b9a05da956a3ce16a180aa8c0df
Gu Donglin <754324585@qq.com>
2021-07-02 09:57:32 +0800

    Generating testing dataset for crowdsourcing

--- router/order.js
+++ router/order.js
@@ -12,7 +12,13 @@ order.get('/list', async (ctx) => {
     let page = params.page
     let limit = params.limit
     let [goods, _] = await pool.query("SELECT g.name, g.price, s.number FROM shopping_cart s JOIN goods g ON s.id = g.id")
+    console.log("=====Checking Goods=====")
+    console.log(page)
+    console.log(limit)
+    console.log(goods)
     goods = goods.filter((value, index) => index < limit * page && index >= limit * (page - 1))
+    console.log("=====After filter=====")
+    console.log(goods)
     let length = goods.length
     let total_prices = 0
     for (let i = 0; i < goods.length; ++i)
@@ -20,6 +26,10 @@ order.get('/list', async (ctx) => {
         total_prices += goods[i].number * goods[i].price
     }
     console.log(goods)
+    console.log("=====Checking=====")
+    console.log(total_prices)
+    console.log(goods)
+    console.log(length)
     ctx.body = {
         code: 20000,
         data: 
