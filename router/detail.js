0ffde939b88c6c67b911eefbf045f90ca804bc4e
tomzzy <tomzzy@live.com>
2021-07-28 22:13:25 +0800

    fix detail page bugs

--- router/detail.js
+++ router/detail.js
@@ -1,7 +1,7 @@
 import Router from 'koa-router'
 import pool from '../sql/pool.js'
 import {parsePostData, parseQueryStr} from '../utils/parsePostData.js'
-import queryPricer from '../queryPricer.cjs'
+import {queryPricer} from '../queryPricer.cjs'
 
 //let queryPricer = require('bindings')('queryPricer.node')
 //import queryPricer from '../build/Release/queryPricer'
@@ -22,11 +22,14 @@ detail.get('/list', async (ctx) => {
     for (let i = 0; i < tb_names.length; ++i)
     {
         let [columns, _2] = await pool.query('SHOW COLUMNS FROM ' + tb_names[i].name)
+        columns = columns.map((value) => value.Field)
+        console.log(columns)
         let types = columns.map(() => 'string')
-        table_info.push({columns, table: tb_names[i], types})
+        table_info.push({columns, table: tb_names[i].name, types})
     }
     let [desc, _3] = await pool.query('SELECT description FROM goods_detail WHERE id = ?', id)
     //console.log(description)
+    console.log(table_info)
     ctx.body = {
         code: 20000,
         data:
@@ -36,10 +39,11 @@ detail.get('/list', async (ctx) => {
         }
     }
 }).get('/check', async (ctx) => {
+    console.log(ctx.query)
     ctx.body = {
         code: 20000,
         data: {
-            status: queryPricer.checkSQL(ctx.query)
+            status: queryPricer.checkSQL(ctx.query.query)
         }
     }
 })
